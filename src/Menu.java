8f20549c0b93a4d3c3e29d091454c76bbfeada85
Mohamed-Ibrahim-M <ibrahimnoorulla133@gmail.com>
2022-01-30 13:19:40 +0530

    adding files

---
+++ src/Menu.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class Menu {
+
+    public static void main(String[] args) {
+
+        HashMap<String,String> customer = new HashMap<String,String>();
+        customer.put("9876543","9876");
+        customer.put("8989898","1890");
+
+        System.out.println("Welcome to the ATM project!");
+
+        System.out.println("Enter Your Customer Number:");
+
+
+        System.out.println();
+
+
+
+    }
+}
